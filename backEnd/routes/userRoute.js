26288bd5aa50246ca96d8b93f456d61b8609d84c
Mohd Ashu <mirza099nadeem.com>
2022-10-15 17:25:27 +0530

    deploy

--- backEnd/routes/userRoute.js
+++ backEnd/routes/userRoute.js
@@ -1,17 +1,17 @@
 // module.exports = router;
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userDb = require('../models/register');
+const userDb = require("../models/register");
 // const { body, validationResult } = require("express-validator");
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 const JWT_SECRET = process.env.SECRET_KEY;
-const jwt = require('jsonwebtoken');
-const fetchuser = require('../middleware/authentication');
-const { json } = require('body-parser');
+const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/authentication");
+const { json } = require("body-parser");
 
 ////register user
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   // check user already there or not
   // console.log(req.body);
   try {
@@ -46,17 +46,17 @@ router.post('/register', async (req, res) => {
       const success = true;
       res.send({ success, authtoken });
     } else {
-      res.status(400).json({ error: 'sorry this user alredy rigistered' });
+      res.status(400).json({ error: "sorry this user alredy rigistered" });
     }
   } catch (err) {
     res
       .status(500)
-      .json({ message: 'internal server error', error: err.message });
+      .json({ message: "internal server error", error: err.message });
   }
 });
 
 ///login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   //checking validation
 
   const { email, password } = req.body;
@@ -66,14 +66,14 @@ router.post('/login', async (req, res) => {
     let success = false;
     //check email present or not
     if (!user) {
-      return res.json({ success, error: 'please enter valid details' });
+      return res.json({ success, error: "please enter valid details" });
     }
 
     //checking password
     const password_compare_res = await bcrypt.compare(password, user.password);
 
     if (!password_compare_res) {
-      return res.json({ success, error: 'invalid password' });
+      return res.json({ success, error: "invalid password" });
     }
 
     const data = {
@@ -86,22 +86,28 @@ router.post('/login', async (req, res) => {
 
     success = true;
 
-    res.json({ success, authtoken, name: user.name.split(' ')[0] });
+    res.json({ success, authtoken, name: user.name.split(" ")[0] });
   } catch (err) {
     // console.log(err);
-    res.status(500).json({ error: 'login server error' });
+    res.status(500).json({ error: "login server error" });
   }
 });
 
-router.get('/verify', fetchuser, async (req, res) => {
+router.get("/verify", fetchuser, async (req, res) => {
   if (req.user) {
-    const data=await userDb.findOne( req.user );
+    const data = await userDb.findOne(req.user);
     res.json({
-      user:req.user,
-      data:data,
-      status: 'success',
+      user: req.user,
+      data: data,
+      status: "success",
     });
   }
 });
-
+router.get("/user", async (req, res) => {
+  const data = await userDb.find();
+  res.json({
+    data: data,
+    status: "success",
+  });
+});
 module.exports = router;
